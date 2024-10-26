c520a491c8d421436d16e1a3fd2faee67bde6f1a
NICOLE CABRERA <nicolecabreralopez@gmail.com>
2024-10-26 11:38:46 -0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
+import "./App.css";
+import { puppyList } from "./data.js";
+import { useState } from "react";
+
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null); // Move this outside of featPup
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  console.log("puppyList: ", puppyList);
+  console.log(featuredPup);
+
+  const handleClick = (id) => {
+    setFeatPupId(id); // Set the clicked puppy's id
+  };
 
   return (
-    <>
-      <div>
-      
+    <div className="App">
+      {puppies.map((puppy) => (
+        <p key={puppy.id} onClick={() => handleClick(puppy.id)}>
+          {puppy.name}
+        </p>
+      ))}
+
+      <div className="featPup">
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
